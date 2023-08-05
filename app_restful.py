94bafeaada55ee2e565e34ee099e8011765d2e6d
LeonardoScatolin <leonardo.scatolin20@gmail.com>
2023-08-05 20:10:25 -0300

    Incluido requirements do projeto, o app_restful com exemplo de aplicação com a extensão flask-restful e incluido o módulo habilidades para gerenciar as habilidades que podem ser associadas ao desenvolvedor

---
+++ app_restful.py
@@ -0,0 +1,59 @@
+from flask import Flask, request
+from flask_restful import Resource, Api
+import json
+from habilidades import Habilidades
+
+app = Flask(__name__)
+api = Api(app)
+
+desenvolvedores = [
+    {
+        'id': '0',
+        'nome': 'Leo',
+        'habilidades' : ['Python', 'Flask']
+    },
+    {
+        'id':'1',
+        'nome': 'Scatolin',
+        'habilidades': ['Python', 'Django']
+    }
+]
+
+# devolve um desenvolvedor pelo ID, também altera e deleta.
+class Desenvolvedor(Resource):
+    def get(self, id):
+        try:
+            response = desenvolvedores[id]
+        except IndexError:
+            response = {'status': 'erro', 'mensagem': 'desenvolvedor de id {} não existe'.format(id)} 
+        except Exception:
+            mensagem = 'Erro desconhecido'
+            response = {'mensagem': mensagem}
+        return response
+    
+    def put(self, id):
+        dados = json.loads(request.data)
+        desenvolvedores[id] = dados
+        return dados
+
+    def delete(self, id):
+        desenvolvedores.pop(id)
+        return {'status':'sucesso', 'mensagem': 'registro excluido'}
+
+# lista todos os desenvolvedores e permite registrar um novo desenvolvedor
+class ListaDesenvolvedores(Resource):
+    def get(self):
+        return desenvolvedores
+    def post(self):
+        dados = json.loads(request.data)
+        posicao = len(desenvolvedores)
+        dados['id'] = posicao
+        desenvolvedores.append(dados)
+        return desenvolvedores[posicao]
+
+api.add_resource(Desenvolvedor, '/dev/<int:id>/')
+api.add_resource(ListaDesenvolvedores, '/dev/')
+api.add_resource(Habilidades, '/habilidades/')
+
+if __name__ == '__main__':
+    app.run(debug=True)
